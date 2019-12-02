f6ec509c69f5cf831a03240cca836d5db0e2de33
DuskoPeric <Dusko.Peric@homegate.ch>
2019-12-02 10:50:25 +0100

    foosball league

---
+++ ivfoosball/precache-manifest.baf097921e711fcacc5fb82e4e09c9f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb97414493d5fda34b14a93de2b652d",
+    "url": "/ivfoosball/index.html"
+  },
+  {
+    "revision": "a2c8abf872ff9e07d57c",
+    "url": "/ivfoosball/static/css/main.6869f0b6.chunk.css"
+  },
+  {
+    "revision": "eaaeb77896c17303ad20",
+    "url": "/ivfoosball/static/js/2.cce22c18.chunk.js"
+  },
+  {
+    "revision": "a2c8abf872ff9e07d57c",
+    "url": "/ivfoosball/static/js/main.17971e5f.chunk.js"
+  },
+  {
+    "revision": "0bcd28a9f913d1265343",
+    "url": "/ivfoosball/static/js/runtime-main.b339a2f3.js"
+  },
+  {
+    "revision": "eec33708b7484b407b94cb98c6693d61",
+    "url": "/ivfoosball/static/media/sch.eec33708.png"
+  },
+  {
+    "revision": "bf2c0a6754cfdb9e02c8487e13165b64",
+    "url": "/ivfoosball/static/media/tab.bf2c0a67.png"
+  }
+]);
